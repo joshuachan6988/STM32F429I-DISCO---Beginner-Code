f88f21e25d4a669c889ca8fff3f65dcf6c9ddfe6
Joshua <jan.joshua@iac.com.tw>
2017-08-08 15:10:18 +0800

    TOUCH_ON
    Using the touch screen function

---
+++ TOUCH_ON/MDK-ARM/TOUCH_ON/ili9341.d
@@ -0,0 +1,4 @@
+touch_on\ili9341.o: ..\Drivers\BSP\Components\ili9341\ili9341.c
+touch_on\ili9341.o: ..\Drivers\BSP\Components\ili9341\ili9341.h
+touch_on\ili9341.o: ..\Drivers\BSP\Components\ili9341\../Common/lcd.h
+touch_on\ili9341.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ TOUCH_ON/MDK-ARM/TOUCH_ON/startup_stm32f429xx.d
@@ -0,0 +1,1 @@
+touch_on\startup_stm32f429xx.o: startup_stm32f429xx.s
