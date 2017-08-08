d2a8df9f3dde65d25b0515ec39396cc75e3b0242
Joshua <jan.joshua@iac.com.tw>
2017-08-08 15:11:09 +0800

    TOUCH_ON
    Using the touch screen fuction

---
+++ TOUCH_ON/MDK-ARM/TOUCH_ON/stmpe811.d
@@ -0,0 +1,5 @@
+touch_on\stmpe811.o: ..\Drivers\BSP\Components\stmpe811\stmpe811.c
+touch_on\stmpe811.o: ..\Drivers\BSP\Components\stmpe811\stmpe811.h
+touch_on\stmpe811.o: ..\Drivers\BSP\Components\stmpe811\../Common/ts.h
+touch_on\stmpe811.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+touch_on\stmpe811.o: ..\Drivers\BSP\Components\stmpe811\../Common/io.h
