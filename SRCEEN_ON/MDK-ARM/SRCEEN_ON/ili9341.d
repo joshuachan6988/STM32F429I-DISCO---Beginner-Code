294a2a1f21792875e42946d61f62e3fe3dc93053
Joshua <jan.joshua@iac.com.tw>
2017-08-08 15:03:50 +0800

    SCREEN_ON
    Print something on the LCD module

---
+++ SRCEEN_ON/MDK-ARM/SRCEEN_ON/ili9341.d
@@ -0,0 +1,4 @@
+srceen_on\ili9341.o: ..\Drivers\BSP\Components\ili9341\ili9341.c
+srceen_on\ili9341.o: ..\Drivers\BSP\Components\ili9341\ili9341.h
+srceen_on\ili9341.o: ..\Drivers\BSP\Components\ili9341\../Common/lcd.h
+srceen_on\ili9341.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
